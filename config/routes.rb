3019fad9aa5048abf71df1e03bd29e37c0838490
Sam Palumbo <samirpalumbo@gmail.com>
2021-10-25 12:44:46 -0300

    add homepage and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   resources :books
   resources :games
   resources :companies
   resources :genres
   resources :platforms
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
