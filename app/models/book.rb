c16db856a4ad414afb7cd0c7cb46c03c2741a019
Sam Palumbo <samirpalumbo@gmail.com>
2021-10-25 12:08:47 -0300

    validate float price

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   belongs_to :genre
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {only_float: true}
   validates :author, presence: true
   validates :description, length: 0..250
   validates :author, length: 3..30
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,6 @@ class Game < ApplicationRecord
   belongs_to :platform
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {only_float: true}
   validates :description, length: 0..250
 end
