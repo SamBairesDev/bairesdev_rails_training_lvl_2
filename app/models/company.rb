d6702d13f24794c77d890bf784ab648e8d261824
Sam Palumbo <samirpalumbo@gmail.com>
2021-10-26 17:42:27 -0300

    fix destroy dependent on company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
-    has_many :games
-    has_many :books
+    has_many :games, dependent: :destroy
+    has_many :books, dependent: :destroy
 
     validates :name, presence: true
 end
