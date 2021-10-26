082204b80835bde63e9cc4d95ef84300cdec14aa
Sam Palumbo <samirpalumbo@gmail.com>
2021-10-26 16:34:40 -0300

    add platform and genre enum and select options

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ApplicationRecord
+    enum name: { adventure: 'adventure', romance: 'romance', scifi: 'scifi', horror: 'horror' }
+    
     has_many :books
 
     validates :name, presence: true
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,4 +1,6 @@
 class Platform < ApplicationRecord
+    enum name: { ps4: 'PS4', xbox: 'XBox', pc: 'PC' }
+
     has_many :games
 
     validates :name, presence: true
