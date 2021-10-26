d4a8fdf70ab1e04ced23ddc1e8506d6821f0de8a
Sam Palumbo <samirpalumbo@gmail.com>
2021-10-26 17:41:17 -0300

    fix destroy dependent on platform and genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,7 @@
 class Genre < ApplicationRecord
     enum name: { adventure: 'adventure', romance: 'romance', scifi: 'scifi', horror: 'horror' }
     
-    has_many :books
+    has_many :books, dependent: :destroy
 
     validates :name, presence: true
 end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,7 +1,7 @@
 class Platform < ApplicationRecord
     enum name: { ps4: 'PS4', xbox: 'XBox', pc: 'PC' }
 
-    has_many :games
+    has_many :games, dependent: :destroy
 
     validates :name, presence: true
 end
